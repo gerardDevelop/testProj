721a43abccfbdf073faa9d8f17a919b330b5a8fc
gerardDevelop <gerard.develop@gmail.com>
2018-12-31 12:16:17 +0700

    change mongo uri

--- databaseServer/config/keys.js
+++ databaseServer/config/keys.js
@@ -1,10 +1,11 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/nodegame',
+  //mongoURI: 'mongodb://localhost:27017/nodegame',
 
   // sudo systemctl start mongod (command to start mongod on local machine)
   // sudo systemctl status mongod
 
 
   //mongoURI: 'mongodb://gerard:gerard1@ds247101.mlab.com:47101/nodegame',   //(mlab)
+  mongoURI: 'mongodb://admin:admin1@ds051863.mlab.com:51863/testdatabase',
   secretOrKey: 'wdecd3v54bije8ove5idf6i5g8'
 };
